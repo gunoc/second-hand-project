7e5034a39f1812e2c8282854adfbe47aa67b92e8
JH <vvaa277@gmail.com>
2023-09-02 21:09:25 +0900

    chore: worker작동하도록 함

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { worker } from './mocks/worker';
 
-if (process.env.USE_MSW === 'true') {
-  worker.start();
-}
+// if (process.env.USE_MSW === 'true') {
+worker.start();
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
